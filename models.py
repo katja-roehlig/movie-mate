cf34202faeb21a9b98bb27d2237462a5eb08b821
Katja Röhlig <katja-roehlig@mail.de>
2026-03-05 18:26:14 +0100

    add basic structure

---
+++ models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    director = db.Column(db.String(255))
+    year = db.Column(db.Integer)
+    img = db.Column(db.String(255))
+    rating = db.Column(db.Real)
